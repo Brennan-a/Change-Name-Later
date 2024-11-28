009a48eccbca2918f65fbbdb4ec7539b077c8f69
Brennan-a <brennie678@gmail.com>
2024-11-28 15:50:50 -0500

    Misc #2

---
+++ thejsway/ch1.js
@@ -0,0 +1,23 @@
+import { cl, title, bigTitle, prompt } from "./util.js";
+bigTitle("Chapter 1: 3, 2, 1... Code");
+{
+  title("Presentation");
+  // Write a program that displays your name and age. Here’s the result for mine.
+  // Put your code here ↓
+  console.log("Nathan");
+  console.log(16);
+  // Put your code here ↑
+}
+
+{
+  title("Minimalistic calculator");
+  // Write a program that displays the results of adding, subtracting, multiplying and dividing 6 by 3.
+  // Always put your code INSIDE the curly braces
+  console.log(6 + 3, 6 - 3, 6 * 3, 6 / 3)
+
+{
+  title("Values prediction");
+  // Observe the following program and try to predict the values it displays.
+  console.log(4 + 5); //9 
+  console.log("4 + 5"); //4
+}
